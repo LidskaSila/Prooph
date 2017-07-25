fd412942d7580e4360909f81b827ba35672faa34
Tomáš Čížek <tomascizek@gmail.com>
2017-07-25 22:18:15 +0200

    fix - now it is possible to reference other services with @serviceLinks in prooph config without causing accidental circular reference problem

--- src/ProophExtension.php
+++ src/ProophExtension.php
@@ -5,6 +5,7 @@ namespace LidskaSila\Prooph;
 use LidskaSila\Prooph\Common\Configurator;
 use LidskaSila\Prooph\Common\NetteContainerWrapper;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Container;
 use Nette\DI\ContainerBuilder;
 
 class ProophExtension extends CompilerExtension
@@ -21,7 +22,7 @@ class ProophExtension extends CompilerExtension
 		$this->configurator = new ProophExtensionConfigurator($this);
 	}
 
-	public function loadConfiguration()
+	public function loadConfiguration(): void
 	{
 		$this->defaults = $this->configurator->buildDefaultConfig();
 		$this->config   = $this->mergeConfigIntoDefaults($this->config, $this->defaults);
@@ -36,22 +37,48 @@ class ProophExtension extends CompilerExtension
 		return array_replace_recursive($default, $original);
 	}
 
+	/**
+	 * Container wrapper is needed in every interop factory, so we register it first.
+	 */
 	private function registerContainerWrapper(): void
 	{
 		/** @var ContainerBuilder $containerBuilder */
 		$containerBuilder = $this->getContainerBuilder();
 
 		if (!$containerBuilder->getByType(NetteContainerWrapper::class)) {
+			$jsonConfig         = $this->determineJsonConfig();
+			$containerServiceId = $this->getContainerServiceId($containerBuilder);
+
 			$containerBuilder
 				->addDefinition($this->getNetteContainerWrapperDefinitionId())
 				->setClass(NetteContainerWrapper::class)
-				->setArguments([ [ $this->configurator->getConfigKey() => $this->config ] ]);
+				->setFactory(static::class . '::createContainerWrapper', [ $jsonConfig, '@' . $containerServiceId ]);
 		}
 	}
 
-	private function getNetteContainerWrapperDefinitionId()
+	public static function createContainerWrapper(string $jsonConfig, $container): NetteContainerWrapper
+	{
+		$config = json_decode($jsonConfig, true);
+
+		return new NetteContainerWrapper($config, $container);
+	}
+
+	private function getNetteContainerWrapperDefinitionId(): string
 	{
 		return $this->prefix('NetteContainerWrapper');
+	}
+
+	private function determineJsonConfig(): string
+	{
+		// Putting config array to json, because we want keep service links (@serviceId) in config as string.
+		// If it would be array, Nette would replace all these string based links with real services.
+		$config     = [ $this->configurator->getConfigKey() => $this->config ];
+		return json_encode($config);
+	}
+
+	private function getContainerServiceId(ContainerBuilder $containerBuilder): string
+	{
+		return $containerBuilder->getByType(Container::class);
 	}
 }
 
